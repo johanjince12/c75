e9f04b35a3814357c079137ef7db4e5ac85b4cfb
johanjince12 <96847741+johanjince12@users.noreply.github.com>
2022-07-22 09:09:05 -0400

    Add files via upload

---
+++ PRO-C75-Template-master/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuzPLyLwQce0kPbeAAYm79k3l5S9R21eY",
+  authDomain: "e-library-3de66.firebaseapp.com",
+  projectId: "e-library-3de66",
+  storageBucket: "e-library-3de66.appspot.com",
+  messagingSenderId: "1037313799470",
+  appId: "1:1037313799470:web:e3615626cc76a31190b6bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
